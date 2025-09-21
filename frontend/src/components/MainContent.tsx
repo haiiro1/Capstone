ef1dc5dcab2cae2a8071bb61b43a9bc69d1f174d
haiiro <107000063+haiiro1@users.noreply.github.com>
2025-09-21 20:55:04 -0300

    summary

--- frontend/src/components/MainContent.tsx
+++ frontend/src/components/MainContent.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 import { type ReactNode } from "react";
+
 interface MainContentProps {
   title: string;
   children: ReactNode;
 }
+
 
 function MainContent({ title, children }: MainContentProps) {
   return (
@@ -10,8 +13,9 @@ function MainContent({ title, children }: MainContentProps) {
       <div className="d-flex justify-content-between align-items-center mb-4">
         <h2 className="fw-bold">{title}</h2>
         <div>
-          <button className="btn btn-light me-2">Feedback</button>
-          <button className="btn btn-light">Ayuda</button>
+          <Link to="/ayuda" className="btn btn-light">
+            Ayuda
+          </Link>
         </div>
       </div>
       <div>{children}</div>
